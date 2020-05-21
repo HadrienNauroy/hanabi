596129cc43b2fc85342d2469edf991e5836b3697
HadrienNauroy <hadrien.nauroy@ensta-paris.fr>
2020-05-21 12:57:41 +0200

    Préparation du rendu
    -ajout de la description de l'interface dans le michier .md
    -amélioration du fichier get_data :+1:
        _passage en fréquence
        _ajout du nom des axes
        _ajout de l'affichage de la moyenne

--- src/get_data.py
+++ src/get_data.py
@@ -5,11 +5,11 @@ import hanabi
 import matplotlib.pyplot as plt
 
 start_time = time.time()
-n=100
+n=1000
 nb_players=5
 results=26*[0]
 
-
+s=0
 for k in range(n):
 	game=hanabi.Game(nb_players)
 	ai=hanabi.ai.Cheater(game)
@@ -18,11 +18,18 @@ for k in range(n):
 	game.run()
 	res=game.score
 	results[res]+=1
+	s+=res
+
+for k in range(26):
+	results[k]=results[k]/n
 
 
 print("Temps d execution pour %d parties : %s secondes " %(n,round(time.time() - start_time,2)))
+print("score moyen : %s" %(s/n))
 target=[k for k in range(26)]
 plt.clf()
 plt.bar(target,results)
+plt.xlabel('score')
+plt.ylabel('frequence')
 plt.show()
 
